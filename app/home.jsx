df4432d5d3094547acf9960ad483fa7c5c187ad4
shruti garg <shrutigarg1312@gmail.com>
2024-06-11 23:42:30 +0530

    Adding equipment details page

---
+++ app/home.jsx
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+import { View, FlatList, Text, Image, Pressable } from "react-native";
+import { NativeWindStyleSheet } from "nativewind";
+
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
+
+const HomeScreen = ({ navigation }) => {
+  const [constructorEquipments] = useState([
+    {
+      id: 1,
+      type: "Earthmover",
+      image: require("../assets/images/earthmover.jpg"),
+    },
+    {
+      id: 2,
+      type: "Backhoe loader",
+      image: require("../assets/images/grader.jpg"),
+    },
+    {
+      id: 3,
+      type: "Grader",
+      image: require("../assets/images/earthmover.jpg"),
+    },
+    {
+      id: 4,
+      type: "Dump Trucks",
+      image: require("../assets/images/grader.jpg"),
+    },
+    {
+      id: 5,
+      type: "Excavators",
+      image: require("../assets/images/earthmover.jpg"),
+    },
+    { id: 6, type: "Cranes", image: require("../assets/images/grader.jpg") },
+    { id: 7, type: "Hyrda", image: require("../assets/images/earthmover.jpg") },
+    {
+      id: 8,
+      type: "Dump Trucks",
+      image: require("../assets/images/grader.jpg"),
+    },
+    {
+      id: 9,
+      type: "Excavators",
+      image: require("../assets/images/earthmover.jpg"),
+    },
+    { id: 10, type: "Cranes", image: require("../assets/images/grader.jpg") },
+    {
+      id: 11,
+      type: "Bulldozers",
+      image: require("../assets/images/earthmover.jpg"),
+    },
+    // Add more equipment types as needed
+  ]);
+
+  const itemPress = (item) => {
+    console.log("item pressed: ", item.type);
+    navigation.navigate("NewItems");
+  };
+
+  const renderItem = ({ item }) => (
+    <Pressable
+      onPress={() => itemPress(item)}
+      style={({ pressed }) => [
+        {
+          backgroundColor: pressed ? "darkgray" : "lightgray",
+        },
+        styles.wrapperCustom,
+      ]}
+    >
+      <Image
+        source={item.image}
+        className="mr-4 w-20 h-20 border-black bg-white"
+      />
+      <Text className="text-lg font-semi-bold">{item.type}</Text>
+    </Pressable>
+  );
+
+  return (
+    <View className="flex-1">
+      <View className="w-full p-3 mb-2 border-b border-gray-600">
+        <Text className="text-2xl font-bold">Rent Mech</Text>
+      </View>
+      <View className="p-4 flex-1">
+        <FlatList
+          data={constructorEquipments}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id.toString()}
+          showsVerticalScrollIndicator={false}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = {
+  wrapperCustom: {
+    flexDirection: "row",
+    padding: 16,
+    marginBottom: 16,
+    borderRadius: 8,
+    alignItems: "center",
+  },
+};
--- app/index.jsx
+++ app/index.jsx
@@ -1,73 +1,31 @@
-import React, { useState } from 'react';
-import {SafeAreaView, View, FlatList, Text, Image, Pressable } from 'react-native';
-import { NativeWindStyleSheet } from "nativewind";
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
 
-NativeWindStyleSheet.setOutput({
-  default: "native",
-});
+import HomeScreen from "./home";
+import NewItemsScreen from "./equipments";
 
-const Home = () => {
-  
-  const [constructorEquipments] = useState([
-    { id: 1, type: 'Earthmover', image: require('../assets/images/earthmover.jpg')},
-    { id: 2, type: 'Backhoe loader', image: require('../assets/images/grader.jpg')},
-    { id: 3, type: 'Grader', image: require('../assets/images/earthmover.jpg')},
-    { id: 4, type: 'Dump Trucks', image: require('../assets/images/grader.jpg')},
-    { id: 5, type: 'Excavators', image: require('../assets/images/earthmover.jpg')},
-    { id: 6, type: 'Cranes', image: require('../assets/images/grader.jpg')},
-    { id: 7, type: 'Hyrda', image: require('../assets/images/earthmover.jpg')},
-    { id: 8, type: 'Dump Trucks', image: require('../assets/images/grader.jpg')},
-    { id: 9, type: 'Excavators', image: require('../assets/images/earthmover.jpg')},
-    { id: 10, type: 'Cranes', image: require('../assets/images/grader.jpg')},
-    { id: 11, type: 'Bulldozers', image: require('../assets/images/earthmover.jpg')}
-    // Add more equipment types as needed
-  ]);
+const Stack = createStackNavigator();
 
-  const itemPress = (item) => {
-    console.log("item pressed: ", item.type);
-  };
-
-  const renderItem = ({ item }) => (
-    <Pressable onPress={() => itemPress(item)}  
-    style={({pressed}) => [
-      {
-        backgroundColor: pressed ? 'darkgray' : 'lightgray',
-      },
-      styles.wrapperCustom,
-    ]}
-    >
-      <Image source={item.image} className="mr-4 w-20 h-20 border-black bg-white" />
-      <Text className="text-lg font-semi-bold">{item.type}</Text>
-    </Pressable>
-  );
-
+const App = () => {
   return (
-    <SafeAreaView className="flex-1">
-      <View className="w-full p-3 mb-2 border-b border-gray-600">
-        <Text className="text-2xl font-bold">Rent Mech</Text>
-      </View>
-      <View className="p-4 flex-1">
-        <FlatList
-          data={constructorEquipments}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.id.toString()}
-          showsVerticalScrollIndicator={false}
-        />
-      </View>
-    </SafeAreaView>
+    <Stack.Navigator
+      screenOptions={{
+        cardStyle: { flex: 1 },
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ title: "Rent Mech" }}
+      />
+      <Stack.Screen
+        name="NewItems"
+        component={NewItemsScreen}
+        options={{ title: "New Items" }}
+      />
+    </Stack.Navigator>
   );
-
 };
 
-export default Home;
-
-
-const styles = {
-  wrapperCustom: {
-    flexDirection: 'row',
-    padding: 16,
-    marginBottom: 16,
-    borderRadius: 8,
-    alignItems: 'center',
-  },
-};
+export default App;
