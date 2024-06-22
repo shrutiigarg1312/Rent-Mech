c311a4e0fd3bd10eb7701ba6524a61e5eeeec04d
Devil-9 <johndoe@example.com>
2024-06-23 03:21:43 +0530

    add send OTP function

--- app/modals/ForgotPasswordModal.jsx
+++ app/modals/ForgotPasswordModal.jsx
@@ -19,8 +19,44 @@ const ForgotPasswordModal = () => {
   } = useAuth();
   const [email, setEmail] = useState("");
   const [error, setError] = useState("");
+  const [otpNumber, setOtpNumber] = useState("");
+  const [otp, setOtp] = useState("");
 
   const navigation = useNavigation();
+
+  const handleSendOTP = async () => {
+    // Generate a random number between 100000 and 999999
+    const OTP = Math.floor(100000 + Math.random() * 900000).toString();
+    setOtpNumber(OTP);
+    console.log(otpNumber);
+    try {
+      const mailResponse = await fetch(
+        "https://rmmail.onrender.com/send-email",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+          body: new URLSearchParams({
+            to: email,
+            subject: "OTP for password change",
+            text: otpNumber,
+          }).toString(),
+        }
+      );
+    } catch (error) {
+      console.error("OTP request failed:", error);
+    }
+  };
+
+  const handleSubmit = () => {
+    if (otp === otpNumber) {
+
+    }
+    else {
+      setError("Wrong OTP");
+    }
+  };
 
   const handleGoBack = () => {
     closeForgotPasswordModal();
@@ -48,11 +84,22 @@ const ForgotPasswordModal = () => {
               keyboardType="email-address"
               autoCapitalize="none"
             />
+            <TextInput
+              style={styles.input}
+              placeholder="OTP"
+              value={otp}
+              onChangeText={setOtp}
+              keyboardType="OTP"
+              autoCapitalize="none"
+            />
             {error ? <Text style={styles.errorText}>{error}</Text> : null}
           </View>
           <View style={styles.footer}>
-            <Pressable style={styles.button}>
-              <Text style={styles.buttonText}>Change Password</Text>
+            <Pressable style={styles.button} onPress={handleSendOTP}>
+              <Text style={styles.buttonText}>Send OTP</Text>
+            </Pressable>
+            <Pressable style={styles.button} onPress={handleSubmit}>
+              <Text style={styles.buttonText}>Submit</Text>
             </Pressable>
             <Pressable style={styles.back} onPress={handleGoBack}>
               <Text>Go back</Text>
