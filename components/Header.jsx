763598d32bbf7f6a4c0b2f3cff9eab517f5037aa
shruti garg <shrutigarg1312@gmail.com>
2024-06-22 11:34:12 +0530

    Make Title/Icon on header navigate to home

--- components/Header.jsx
+++ components/Header.jsx
@@ -11,12 +11,18 @@ const Header = () => {
     navigation.openDrawer();
   };
 
+  const navigateToHome = () => {
+    navigation.navigate("Home");
+  };
+
   return (
     <View className="flex-row items-center w-full p-2 pl-5 bg-primary shadow-lg z-1 ">
       <Pressable onPress={openDrawer} style={{ padding: 10 }}>
         <Ionicons name="menu" size={24} color="#f0f0f0" />
       </Pressable>
-      <Text className="px-5 text-2xl font-bold text-white">Rent Mech</Text>
+      <Pressable onPress={navigateToHome}>
+        <Text className="px-5 text-2xl font-bold text-white">Rent Mech</Text>
+      </Pressable>
       <View className="absolute right-2 ">
         <LocationSelector />
       </View>
