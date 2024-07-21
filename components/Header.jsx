ee6d77b4944254ea9d4a3387f738ea2aed934f47
shruti garg <shrutigarg1312@gmail.com>
2024-07-21 19:38:41 +0530

    Added App Logo

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { View, Text, Pressable, Picker } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, Pressable, Image, Dimensions } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import LocationSelector from "./LocationSelector";
@@ -20,14 +20,24 @@ const Header = () => {
       <Pressable onPress={openDrawer} style={{ padding: 10 }}>
         <Ionicons name="menu" size={24} color="#f0f0f0" />
       </Pressable>
-      <Pressable onPress={navigateToHome}>
-        <Text className="px-5 text-2xl font-bold text-white">Rent Mech</Text>
+      <Pressable className="flex-row items-center p-2" onPress={navigateToHome}>
+        <Image
+          style={styles.logo}
+          source={require("../assets/images/rentmech-logo-title.png")}
+        />
       </Pressable>
       <View className="absolute right-2 ">
         <LocationSelector />
       </View>
     </View>
   );
+};
+
+const styles = {
+  logo: {
+    width: 150,
+    height: 25,
+  },
 };
 
 export default Header;
